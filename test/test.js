66faaeeb8ece8f739205ebb75b69ee1e2a888325
Zordius Chen <zordius@yahoo-inc.com>
2014-10-16 18:07:45 +0800

    test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('chai').assert,
+    ST = require('../subtask.js');
+
+describe('subtask', function () {
+    it('should be a function', function (done) {
+        assert('function', typeof ST);
+        done();
+    });
+});
