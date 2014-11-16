acb734ca11a7cece110978fac5e0cf5d93d549a2
Zordius Chen <zordius@yahoo-inc.com>
2014-11-16 12:13:35 +0800

    move

---
+++ test/test-pick.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var assert = require('chai').assert,
+    ST = require('../subtask.js');
+
+describe('subtask.pick', function () {
+    it('should get result by path', function (done) {
+        ST({
+            a: {
+                b: 'no',
+                c: 'OK!'
+            },
+            'a.c': 'no'
+        })
+        .pick('a.c')
+        .execute(function (D) {
+            assert.equal('OK!', D);
+            done();
+        });
+    });
+
+    it('should be safe when pick from undefined', function (done) {
+        ST().pick('a.c')
+        .execute(function (D) {
+            assert.equal(undefined, D);
+            done();
+        });
+    });
+});
